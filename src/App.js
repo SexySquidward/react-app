d2688e206b5b3de3db4f34d551c3e0855e344525
Jason <jason.stuart27@gmail.com>
2023-02-22 18:49:21 +1030

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import Login from "./login";
+import PasswordPage from "./passPage";
+import {AnimatePresence} from "framer-motion"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+function App() {
+  return (
+    
+    <BrowserRouter>
+    <AnimatePresence exitBeforeEnter>
+      <Routes>
+        <Route  index  element={<Login />}/>
+        <Route  path="pwd"  element={<PasswordPage />}/>
+        </Routes>
+      </AnimatePresence>
+    </BrowserRouter>
+    
+   
+    
+  );
+}
+
+export default App;
